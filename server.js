8258ed0cd7ba7e550d008e4be72ec8b5cd7f01aa
Hardik Shandilya <hardikshandilya021@gmail.com>
2025-03-31 09:32:41 +0530

    added / route

--- server.js
+++ server.js
@@ -35,6 +35,10 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+    res.status(200).send('Service is running');
+  });
+
 // Define routes
 app.post('/api/setname',VerifyToken, SetName);
 app.post('/api/chat',VerifyToken, BotChat);
